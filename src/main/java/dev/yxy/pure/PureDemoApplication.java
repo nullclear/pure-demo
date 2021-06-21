c3da0a5672dd222ca88f239b728a542ddb972834
nullclear <1206134796@qq.com>
2021-06-22 00:20:51 +0800

    release(1.0.0): pure demo

---
+++ src/main/java/dev/yxy/pure/PureDemoApplication.java
@@ -0,0 +1,12 @@
+package dev.yxy.pure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PureDemoApplication.class, args);
+    }
+}
